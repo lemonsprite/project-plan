24415587162e99149096b40a9da1b4daf10c99f8
lemonsprite <alfath.noor17@gmail.com>
2022-03-22 20:57:24 +0700

    init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,5 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  Home
 </template>
 
 <script>
@@ -11,8 +8,5 @@ import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
 }
 </script>
