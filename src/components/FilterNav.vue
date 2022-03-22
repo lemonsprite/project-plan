17956f858d32139869e9217a95dd60696b8ec7e3
lemonsprite <alfath.noor17@gmail.com>
2022-03-23 00:35:27 +0700

    finishing aplikasi menggunakan filter

---
+++ src/components/FilterNav.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="filter-nav">
+    <button
+      @click="updateFilter('all')"
+      :class="{ active: filterInit === 'all' }"
+    >
+      Lihat Semua
+    </button>
+    <button
+      @click="updateFilter('completed')"
+      :class="{ active: filterInit === 'completed' }"
+    >
+      Selesai
+    </button>
+    <button
+      @click="updateFilter('ongoing')"
+      :class="{ active: filterInit === 'ongoing' }"
+    >
+      Ongoing
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["filterInit"],
+  methods: {
+    updateFilter(params) {
+      this.$emit("filter", params);
+    },
+  },
+};
+</script>
+<style>
+.filter-nav button {
+  background: none;
+  border: none;
+  color: #bbb;
+  outline: none;
+  font-size: 12px;
+  text-transform: uppercase;
+  margin-right: 10px;
+  letter-spacing: 1px;
+  font-weight: bold;
+  cursor: pointer;
+}
+.filter-nav button.active {
+  color: #555;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,46 +1,66 @@
 <template>
   <div class="home">
     <div v-if="projects.length">
-      <div v-for="project in projects" :key="project.id">
-        <SingleProject :project="project" @delete="handleDelete" @done="handleDone"></SingleProject>
+      <FilterNav @filter="filterInit = $event" :filterInit="filterInit" />
+      <div v-for="project in filteredData" :key="project.id">
+        <SingleProject
+          :project="project"
+          @delete="handleDelete"
+          @done="handleDone"
+        ></SingleProject>
       </div>
     </div>
   </div>
-
 </template>
 
 <script>
-import SingleProject from '@/components/SingleProject.vue'
+import SingleProject from "@/components/SingleProject.vue";
+import FilterNav from "@/components/FilterNav.vue";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    SingleProject
+    SingleProject,
+    FilterNav,
   },
   data() {
     return {
       projects: [],
-    }
+      filterInit: "all",
+    };
   },
   mounted() {
-    fetch('http://localhost:3000/projects')
-    .then(res => res.json())
-    .then(data => this.projects = data)
-    .catch(err => console.log(err.message))
+    fetch("http://localhost:3000/projects")
+      .then((res) => res.json())
+      .then((data) => (this.projects = data))
+      .catch((err) => console.log(err.message));
   },
   methods: {
     handleDelete(id) {
       this.projects = this.projects.filter((p) => {
-        return p.id !== id
-      })
+        return p.id !== id;
+      });
     },
     handleDone(id) {
-      let p = this.projects.find(project => {
-        return project.id === id
-      })
+      let p = this.projects.find((project) => {
+        return project.id === id;
+      });
 
-      p.complete = !p.complete
+      p.complete = !p.complete;
+    },
+  },
+  computed: {
+    filteredData() {
+      if(this.filterInit === 'completed') {
+        return this.projects.filter((p) => p.complete)
+      }
+
+      if(this.filterInit === 'ongoing') {
+        return this.projects.filter((p) => !p.complete)
+      }
+      
+      return this.projects
     }
   }
-}
+};
 </script>
